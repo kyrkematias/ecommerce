d2e3797384e291b8fe6a0d0f40d58115e87315bb
Kyrke <martin_matias@hotmail.com.ar>
2023-04-12 14:13:02 -0300

    API + Schema cration

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import { Products } from "../components/Products";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   return (
     <>
       <main className={styles.main}>
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-const mongoose = require('mongoose');
 const connection = {};
 
 async function connect() {
